5dd16ebff55519e4d0c14815c9e7dc7bdb6f270d
Elena Pakulina <el.pakulina@yandex.ru>
2024-02-09 14:30:40 +0500

    2.0. FileOutputStream [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,18 @@
+package ru.job4j.io;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream stream = new FileOutputStream("data/dataresult.txt")) {
+            for (int i = 1; i < 10; i++) {
+                stream.write(String.format("1 * %d = %d", i, i).getBytes());
+                stream.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
