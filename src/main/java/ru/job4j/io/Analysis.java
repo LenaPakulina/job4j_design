4871b3aeb9b26eeee386fd9f8ed4478e616580f0
Elena Pakulina <el.pakulina@yandex.ru>
2024-02-10 20:35:33 +0500

    7. Анализ доступности сервера [#859]

---
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -0,0 +1,37 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Analysis {
+    public void unavailable(String source, String target) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(source));
+             PrintWriter print = new PrintWriter(
+                     new BufferedOutputStream(new FileOutputStream(target)))) {
+            List<String> lines = reader.lines().toList();
+            String firstTime = null;
+            for (int i = 0; i < lines.size(); i++) {
+                String[] array = lines.get(i).split(" ");
+                if (array.length > 1) {
+                    if (firstTime == null
+                            && ("400".equals(array[0]) || "500".equals(array[0]))) {
+                        firstTime = array[1];
+                    } else if (firstTime != null
+                            && ("200".equals(array[0]) || "300".equals(array[0]))) {
+                        print.println(firstTime + ";" + array[1] + ";");
+                        firstTime = null;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Analysis analysis = new Analysis();
+        analysis.unavailable("data/server.log", "data/target.csv");
+        analysis.unavailable("data/server1.log", "data/target1.csv");
+    }
+}
