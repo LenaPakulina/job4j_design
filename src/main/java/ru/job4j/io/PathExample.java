53ebe43f12a0e4e45a54d6c5207c720c7fb47c68
Elena Pakulina <el.pakulina@yandex.ru>
2024-02-20 11:02:53 +0500

    9.1. Файлы и директории. Классы Path, Files, Paths [#504936]

---
+++ src/main/java/ru/job4j/io/PathExample.java
@@ -0,0 +1,29 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathExample {
+    public static void main(String[] args) throws IOException {
+        Path directory = Paths.get("path/paths");
+        Files.createDirectories(directory);
+        Path path = Path.of("path/paths/path.txt");
+        Files.createFile(path);
+        Files.delete(directory);
+    }
+
+    public void example() throws IOException {
+        Path directory = Paths.get("path/paths");
+        Files.createDirectories(directory);
+        Path target = Paths.get("path");
+        Path pathOne = Path.of("path/paths/path1.txt");
+        Files.createFile(pathOne);
+        Path pathTwo = Path.of("path/path2.txt");
+        Files.createFile(pathTwo);
+        DirectoryStream<Path> paths = Files.newDirectoryStream(target);
+        paths.forEach(System.out::println);
+    }
+}
