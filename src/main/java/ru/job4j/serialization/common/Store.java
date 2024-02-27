49a5f49b7089ea0ee2be421fdbaf6bf89fc969e5
Elena Pakulina <el.pakulina@yandex.ru>
2024-02-27 17:53:18 +0500

    5. Преобразование JSON в POJO. JsonObject [#315064]

--- src/main/java/ru/job4j/serialization/common/Store.java
+++ src/main/java/ru/job4j/serialization/common/Store.java
@@ -2,15 +2,21 @@ package ru.job4j.serialization.common;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import ru.job4j.serialization.xml.Contact;
+import ru.job4j.serialization.xml.Person;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Store {
-    private final boolean isOpen;
-    private final int employeesCount;
-    private final String name;
-    private final StoreManager storeManager;
-    private final String[] products;
+    private boolean isOpen;
+    private int employeesCount;
+    private String name;
+    private StoreManager storeManager;
+    private String[] products;
 
     public Store(boolean isOpen, int employeesCount, String name, StoreManager storeManager, String[] products) {
         this.isOpen = isOpen;
@@ -31,13 +37,52 @@ public class Store {
                 + '}';
     }
 
-    public static void main(String[] args) {
+    public boolean isOpen() {
+        return isOpen;
+    }
+
+    public void setOpen(boolean open) {
+        isOpen = open;
+    }
+
+    public int getEmployeesCount() {
+        return employeesCount;
+    }
+
+    public void setEmployeesCount(int employeesCount) {
+        this.employeesCount = employeesCount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public StoreManager getStoreManager() {
+        return storeManager;
+    }
+
+    public void setStoreManager(StoreManager storeManager) {
+        this.storeManager = storeManager;
+    }
+
+    public String[] getProducts() {
+        return products;
+    }
+
+    public void setProducts(String[] products) {
+        this.products = products;
+    }
+
+    public static void func(String[] args) {
         final Store store = new Store(true, 10, "Roga",
                 new StoreManager("Ga", 5),
                 new String[] {"Milk", "Bread"});
         final Gson gson = new GsonBuilder().create();
         System.out.println(gson.toJson(store));
-
         final String storeStr =
                 "{"
                         + "\"isOpen\":false,"
@@ -53,6 +98,31 @@ public class Store {
                         + "}";
         final Store storeMod = gson.fromJson(storeStr, Store.class);
         System.out.println(storeStr);
+    }
+
+    public static void main(String[] args) {
+        JSONObject jsonStoreManager = new JSONObject("{\"name\":\"Polly\"}");
+
+        List<String> list = new ArrayList<>();
+        list.add("Milk");
+        list.add("Bread");
+        JSONArray jsonProducts = new JSONArray(list);
+
+        /* JSONObject напрямую методом put */
+        final Store store = new Store(false, 30, "Billy",
+                new StoreManager("Ien", 3), new String[] {"Milk", "Bread"});
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("isOpen", store.isOpen());
+        jsonObject.put("employeesCount", store.getEmployeesCount());
+        jsonObject.put("name", store.getName());
+        jsonObject.put("storeManager", jsonStoreManager);
+        jsonObject.put("products", jsonProducts);
+
+        /* Выведем результат в консоль */
+        System.out.println(jsonObject.toString());
+
+        /* Преобразуем объект person в json-строку */
+        System.out.println(new JSONObject(store).toString());
     }
 }
 
--- src/main/java/ru/job4j/serialization/xml/Person.java
+++ src/main/java/ru/job4j/serialization/xml/Person.java
@@ -1,11 +1,16 @@
 package ru.job4j.serialization.xml;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.annotation.*;
 import java.io.StringWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @XmlRootElement(name = "person")
 @XmlAccessorType(XmlAccessType.FIELD)
@@ -42,7 +47,7 @@ public class Person {
                 + '}';
     }
 
-    public static void main(String[] args) throws JAXBException {
+    public static void func(String[] args) throws JAXBException {
 
         final Person person = new Person(false, 30, new Contact("11-111"), "Worker", "Married");
 
@@ -57,5 +62,63 @@ public class Person {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+
+        /* JSONObject из json-строки строки */
+        JSONObject jsonContact = new JSONObject("{\"phone\":\"+7(924)111-111-11-11\"}");
+
+        /* JSONArray из ArrayList */
+        List<String> list = new ArrayList<>();
+        list.add("Student");
+        list.add("Free");
+        JSONArray jsonStatuses = new JSONArray(list);
+
+        /* JSONObject напрямую методом put */
+        final Person person = new Person(false, 30, new Contact("11-111"), "Worker", "Married");
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("sex", person.getSex());
+        jsonObject.put("age", person.getAge());
+        jsonObject.put("contact", jsonContact);
+        jsonObject.put("statuses", jsonStatuses);
+
+        /* Выведем результат в консоль */
+        System.out.println(jsonObject.toString());
+
+        /* Преобразуем объект person в json-строку */
+        System.out.println(new JSONObject(person).toString());
+    }
+
+    public boolean getSex() {
+        return sex;
+    }
+
+    public void setSex(boolean sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public void setContact(Contact contact) {
+        this.contact = contact;
+    }
+
+    public String[] getStatuses() {
+        return statuses;
+    }
+
+    public void setStatuses(String[] statuses) {
+        this.statuses = statuses;
     }
 }
