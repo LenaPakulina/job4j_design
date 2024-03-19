732f2c68473dd21142354e58757929487daf22d8
Elena Pakulina <el.pakulina@yandex.ru>
2024-03-19 13:57:16 +0500

    0. Принцип единственной ответственности [#4913] 2

--- src/main/java/ru/job4j/ood/srp/violation/Finder.java
+++ src/main/java/ru/job4j/ood/srp/violation/Finder.java
@@ -1,4 +1,4 @@
-package ru.job4j.ood.srp;
+package ru.job4j.ood.srp.violation;
 
 /*
 Класс содержит нарушение принципа SRP
--- src/main/java/ru/job4j/ood/srp/violation/Printer.java
+++ src/main/java/ru/job4j/ood/srp/violation/Printer.java
@@ -1,4 +1,4 @@
-package ru.job4j.ood.srp;
+package ru.job4j.ood.srp.violation;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
