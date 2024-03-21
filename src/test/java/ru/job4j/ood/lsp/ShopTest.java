91d76442d5bb4a6869b5cb8ecf1a8765fc8ab048
Elena Pakulina <el.pakulina@yandex.ru>
2024-03-21 14:14:42 +0500

    1. Хранилище продуктов [#852 #487231]

--- src/test/java/ru/job4j/ood/lsp/ShopTest.java
+++ src/test/java/ru/job4j/ood/lsp/ShopTest.java
@@ -13,7 +13,7 @@ class ShopTest {
     @Test
     public void checkDiscount() {
         AbstractStore shop = new Shop();
-        Food product = new Bread("White", LocalDate.now().plusDays(10), LocalDate.now().plusDays(8), 80.0, 0.5);
+        Food product = new Bread("White", LocalDate.now().plusDays(2), LocalDate.now().minusDays(10), 80.0, 0.5);
         assertTrue(shop.supports(product));
         assertEquals(72.0, product.getPrice(), 0.01);
     }
@@ -21,7 +21,7 @@ class ShopTest {
     @Test
     public void checkByDate() {
         AbstractStore shop = new Shop();
-        Food product = new Bread("White", LocalDate.now().plusDays(10), LocalDate.now(), 80.0, 0.2);
+        Food product = new Bread("White", LocalDate.now().plusDays(10), LocalDate.now().minusDays(5), 80.0, 0.2);
         assertTrue(shop.supports(product));
         assertEquals(product.getPrice(), product.getPrice(), 0.01);
     }
